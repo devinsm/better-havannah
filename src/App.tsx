2a2415e0c4e8978d43422a1bbfce524d8d742c76
Devin McAllester <devinmcallester@gmail.com>
2020-03-08 16:15:38 -0700

    Reizes board as screen size changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Board from 'components/Board';
 import { makeStyles, createStyles } from '@material-ui/core/styles';
+import debounce from 'lodash/debounce';
 
+const MAX_CONTENT_WIDTH_PX = 1100;
 const useAppStyles = makeStyles(theme =>
   createStyles({
     root: {
@@ -12,20 +14,37 @@ const useAppStyles = makeStyles(theme =>
       justifyContent: 'center'
     },
     content: {
-      maxWidth: '1100px',
+      maxWidth: `${MAX_CONTENT_WIDTH_PX}px`,
       display: 'flex',
       flexDirection: 'column',
       alignItems: 'center'
     }
   })
 );
+
 const App: React.ComponentType = () => {
   const styleClasses = useAppStyles();
+  const [boardWidthPx, setBoardWidth] = useState(MAX_CONTENT_WIDTH_PX);
+  useEffect(() => {
+    const adjustBoardWidth: () => void = debounce(
+      () => {
+        const viewPortWidthPx = document.documentElement.clientWidth;
+        const newWidth = Math.min(MAX_CONTENT_WIDTH_PX, viewPortWidthPx);
+        setBoardWidth(newWidth);
+      },
+      50,
+      {
+        maxWait: 500
+      }
+    );
+    window.addEventListener('resize', adjustBoardWidth);
+    return (): void => window.removeEventListener('resize', adjustBoardWidth);
+  });
   return (
     <div className={styleClasses.root}>
       <div className={styleClasses.content}>
         <h1>Havannah</h1>
-        <Board widthPx={1100} />
+        <Board widthPx={boardWidthPx} />
       </div>
     </div>
   );
