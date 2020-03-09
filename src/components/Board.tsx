2ac20e59f25b58ca2dd04e7e3c1fe1f0e32daebc
Devin McAllester <devinmcallester@gmail.com>
2020-03-08 17:55:35 -0700

    Adds elevation ~8 box shadow to the board

--- src/components/Board.tsx
+++ src/components/Board.tsx
@@ -3,13 +3,24 @@ import Coordinate from 'models/Coordinate';
 import { observer } from 'mobx-react';
 import { ServiceContext } from 'services/ServiceContainer';
 import Cell from './Cell';
+import { makeStyles, createStyles } from '@material-ui/core/styles';
 import * as utils from 'utils';
+
+const useBoardStyles = makeStyles(() =>
+  createStyles({
+    root: {
+      filter:
+        'drop-shadow(0px 5px 5px rgba(0, 0, 0, 0.2)) drop-shadow(0px 8px 10px rgba(0,0,0,0.14)) drop-shadow(0px 3px 14px rgba(0,0,0,0.12))'
+    }
+  })
+);
 
 export interface BoardProps {
   widthPx: number;
 }
 
 const Board: React.ComponentType<BoardProps> = ({ widthPx }: BoardProps) => {
+  const styleClasses = useBoardStyles();
   const { gameController } = useContext(ServiceContext);
   const boardSize = gameController!.boardSize;
   const coordinates: Coordinate[] = utils.getCoordinates(boardSize);
@@ -33,6 +44,7 @@ const Board: React.ComponentType<BoardProps> = ({ widthPx }: BoardProps) => {
 
   return (
     <svg
+      className={styleClasses.root}
       aria-label="Game Board"
       role="group"
       width={`${widthPx}px`}
